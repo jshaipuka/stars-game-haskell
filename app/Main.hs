493eef1e44a933cbbbece3873db2eef964002dbc
jelena <jelenashaipuka@gmail.com>
2022-10-29 18:59:35 +0300

    Guess in loop

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,13 @@ import Lib
 
 main :: IO ()
 main = do
-  print (hasWon 2 3)
-  print (checkGuess 42 3)
-  putStrLn (createStarsOfLen 5)
+  makeGuessInLoop makeGuess
+  putStrLn "Starting game"
+
+--  putStrLn "Starting game"
+--  none <- print 4
+--  answer <- isGoodChar getChar
+--  print answer
+--  print (hasWon 2 3)
+--  print (checkGuess 42 3)
+--  putStrLn (createStarsOfLen 5)
--- out/production/stars-game/Lib.hs
+++ out/production/stars-game/Lib.hs
@@ -1,10 +1,13 @@
 module Lib
-    (
-      hasWon,
-      getDistance,
-      checkGuess,
-      createStarsOfLen
-    ) where
+  ( hasWon,
+    getDistance,
+    checkGuess,
+    createStarsOfLen,
+    isGoodChar,
+    makeGuess,
+    makeGuessInLoop
+  )
+where
 
 hasWon :: Int -> Int -> Bool
 hasWon x y | x == y = True
@@ -25,3 +28,29 @@ checkGuess target guess = getDistance (abs (guess - target))
 createStarsOfLen :: Int -> String
 createStarsOfLen 0 = ""
 createStarsOfLen n = "*" ++ createStarsOfLen (n - 1)
+
+isGoodCharPure :: Char -> IO Bool
+isGoodCharPure 'a' = pure True
+isGoodCharPure _ = pure False
+
+--isGoodCharPure :: Char -> Bool
+--isGoodCharPure 'a' = True
+--isGoodCharPure _ = False
+--
+isGoodChar :: IO Char -> IO Bool
+isGoodChar c = do
+  v <- c
+  isGoodCharPure v
+
+makeGuess :: IO Int
+makeGuess = do
+    putStrLn "Make a guess:"
+    input <- getLine
+    return (read input)
+
+makeGuessInLoop :: IO Int -> IO ()
+makeGuessInLoop c = do
+  v <- c
+  if v == 0 then putStrLn "Thank you for playing!" else makeGuessInLoop makeGuess
+--makeGuessInLoop n = do
+--  makeGuessInLoop makeGuess
