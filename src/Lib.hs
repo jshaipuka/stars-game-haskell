bb2ab893f1ce8bb1e803200b59001dbffbe17e5d
jelena <jelenashaipuka@gmail.com>
2022-10-29 17:09:58 +0300

    First functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,27 @@
 module Lib
-    ( someFunc
+    (
+      hasWon,
+      getDistance,
+      checkGuess,
+      createStarsOfLen
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+hasWon :: Int -> Int -> Bool
+hasWon x y | x == y = True
+hasWon _ _ = False
+
+getDistance :: Int -> Int
+getDistance distance | distance >= 64 = 1
+getDistance distance | distance >= 32 = 2
+getDistance distance | distance >= 16 = 3
+getDistance distance | distance >= 8 = 4
+getDistance distance | distance >= 4 = 5
+getDistance distance | distance >= 2 = 6
+getDistance _ = 7
+
+checkGuess :: Int -> Int -> Int
+checkGuess target guess = getDistance (abs (guess - target))
+
+createStarsOfLen :: Int -> String
+createStarsOfLen 0 = ""
+createStarsOfLen n = "*" ++ createStarsOfLen (n - 1)
